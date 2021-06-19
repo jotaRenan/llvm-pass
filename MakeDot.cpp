857fcebf755ba1d74c2e5103d8dbd8e61370fae3
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-06-19 17:51:34 -0300

    update to match hello example

--- MakeDot.cpp
+++ MakeDot.cpp
@@ -12,23 +12,31 @@
 
 using namespace llvm;
 
-struct MakeDot : public ModulePass {
-    bool runOnModule (Module &M) {
-        std::string file_name = "teste.dot";
-        std::ofstream output(file_name);
-        output << "digraph \"CFG for '" << F.getName().str() << "' function\" {\n";
-        for (Function &F: M) {
-          for (BasicBlock &BB: F) {
-            output << BB.getName().str() << "\n";
-          }
-        }
-        output << "}\n";
-        output.close();
-    
-        return false; // always return false as it does not modify the module
-    }
+#define DEBUG_TYPE "MakeDot"
 
+namespace {
+  struct MakeDot : public ModulePass {
+      static char ID;
+      MakeDot() : ModulePass(ID) {}
+
+      bool runOnModule (Module &M) override {
+          std::string file_name = "teste.dot";
+          std::ofstream output(file_name);
+          for (Function &F: M) {
+            output << "digraph \"CFG for '" << F.getName().str() << "' function\" {\n";
+            for (BasicBlock &BB: F) {
+              output << BB.getName().str() << "\n";
+            }
+          }
+          output << "}\n";
+          output.close();
+      
+          return false; // always return false as it does not modify the module
+      }
+
+  };
 }
 
+
 char MakeDot::ID = 0;
-static RegisterPass<MakeDotProject> X("MakeDot", "Convert bytecode to dot");
+static RegisterPass<MakeDot> X("MakeDot", "Convert bytecode to dot");
