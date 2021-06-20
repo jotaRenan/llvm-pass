11337762f37d8e0d28af8814620abafc94a3362c
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-06-20 12:42:10 -0300

    add instrunction information

--- MakeDot.cpp
+++ MakeDot.cpp
@@ -8,6 +8,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Constants.h"
 
 
 using namespace llvm;
@@ -17,21 +18,53 @@ using namespace llvm;
 namespace {
   struct MakeDot : public ModulePass {
       static char ID;
-      MakeDot() : ModulePass(ID) {}
+      int anonymousInstCounter;
+      MakeDot() : ModulePass(ID) {
+        anonymousInstCounter = 0;
+      }
 
       void createEdges() {}
 
-      void formatInstruction(Instruction &inst) { 
+      std::string createInstructionName() {
+        std::string name = '%' + std::to_string(anonymousInstCounter);
+        anonymousInstCounter++;
+        return name;
+      }
+
+      std::stringstream formatOperand(Value* operand) {
+        std::stringstream fOp;
+        if (operand->hasName()) { // outputs operand name if it exists
+          fOp << " " << operand->getName().str();
+        } else if (ConstantInt *constant = dyn_cast<ConstantInt>(operand)) { // else outputs value if operand is a constant
+          fOp << " " << constant->getZExtValue();
+        }
+        return fOp;
+      }
+
+      std::stringstream formatInstruction(Instruction &inst) { 
+        std::stringstream fInst;
+        if (!inst.hasName() && !inst.getType()->isVoidTy()) { // if instruction does not have a name and is not void create a name for it
+          std::string instName = createInstructionName();
+          inst.setName(instName); // set instruction name so it can be referenced later
+        }
+        if (inst.hasName()) { // output instruction name if it exists
+          fInst << inst.getName().str() << " = ";
+        }
+        fInst << inst.getOpcodeName(); // output opcode
+        for(unsigned int i=0; i < inst.getNumOperands(); i++) {	// output instruction's operands
+          Value* operand = inst.getOperand(i);
+          fInst << formatOperand(operand).rdbuf();
+        }
+        return fInst;
       }
 
       std::stringstream basicBlockToNode(BasicBlock &basic_block) {
         std::string bb_name = basic_block.getName().str(); // gets basic block name as identifier, as it is unique
         std::stringstream node;
-        node << bb_name << "[shape=record,label=\"{" << bb_name << ":\\l\\l\n";
-        for (const Instruction &inst : basic_block ) {
-          node << "\\l";
+        node << '\t' << bb_name << "[shape=record,\n\t\tlabel=\"{" << bb_name << ":\\l\\l\n";
+        for (Instruction &inst : basic_block) {
+          node << "\t\t\t" << formatInstruction(inst).rdbuf() << " \\l\n";
         }
-        node << "}\"];\n";
         return node;
       }
 
@@ -46,7 +79,8 @@ namespace {
             output << "digraph \"CFG for '" << function.getName().str() << "' function\" {\n";
             for (BasicBlock &basic_block: function) {
               output << basicBlockToNode(basic_block).rdbuf();
-              
+              output << "\t\t}\"\n";
+              output << "\t];\n";
             }
             output << "}\n";
           }
